d903403250c381a618d1ea9ec46142f1af242d7c
valentijn <9021853@student.zadkine.nl>
2024-09-16 14:11:07 +0200

    opdracht 4

---
+++ useeffect/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Clock from './components/Clock';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <h1>React Clock</h1>
+    //   <Clock/>
+    // </div>
+    <>
+    <Clock/>
+    </>
+  );
+}
+
+export default App;
