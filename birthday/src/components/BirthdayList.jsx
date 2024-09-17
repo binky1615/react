2bad11fa623014101918a3817c642c4510138515
valentijn <9021853@student.zadkine.nl>
2024-09-17 14:13:52 +0200

    opdracht 6 Birthday

--- birthday/src/components/BirthdayList.jsx
+++ birthday/src/components/BirthdayList.jsx
@@ -8,54 +8,57 @@ const BirthdayList = () => {
     {
       id: 1,
       name: 'Monkey D. kiwi',
-      age: 29,
+      age: 19,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
     },
     {
       id: 2,
-      name: 'Hester Hogan',
-      age: 32,
+      name: 'Clou D. Pete',
+      age: 85,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
     },
     {
       id: 3,
-      name: 'Jason Little',
-      age: 36,
+      name: 'Marshall D. Jason',
+      age: 40,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     },
     {
       id: 4,
-      name: 'Sean Johnson',
-      age: 34,
+      name: 'Netafari D. Gwen',
+      age: 18,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     },
     {
       id: 5,
-      name: 'Portgas D. faith',
-      age: 29,
+      name: 'Portgas D. Faith',
+      age: 19,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     },
   ]);
 
   return (
-      <div className="birthdayList">
-        <h2>birthdays!!</h2>
-        {
-          birthdays.map((birthday) => (
-            <Birthday
-              id={birthday.id}
-              image={birthday.image}
-              name={birthday.name}
-              age={birthday.age}
-            />
-          ))
-        }
-      </div>
+    <div className="birthdayList">
+      <h2>Birthdays of the D!!</h2>
+      {
+       birthdays && birthdays.map((birthday) => (
+          <Birthday
+            id={birthday.id}
+            image={birthday.image}
+            name={birthday.name}
+            age={birthday.age}
+          />
+        ))
+      }
+      <button onClick={() => setBirthday("")}>
+        Remove All
+      </button>
+    </div>
   );
 }
 
